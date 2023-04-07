a49050705663405863ad19f51bbedab66ff1bab5
Jeremy Díaz <jeremy664k@gmail.com>
2023-04-07 13:59:54 -0400

    Add handle submit to resolve ai

--- src/components/FormMath.tsx
+++ src/components/FormMath.tsx
@@ -1,9 +1,18 @@
 import MathField from "../elements/MathField";
 import StyledButton from "../styled/StyledButton";
 
-const FormMath = () => {
+interface FormMathProps {
+    resolveAI: () => void;
+}
+
+const FormMath = ({ resolveAI }: FormMathProps) => {
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        resolveAI();
+    };
+
     return (
-        <form className="flex gap-4">
+        <form className="flex gap-4" onSubmit={handleSubmit}>
             <MathField />
             <StyledButton>Enviar</StyledButton>
         </form>
