6c43e8dff69e65e8aa27b1b789fcfe6d8f95e946
Jeremy Díaz <jeremy664k@gmail.com>
2023-04-07 14:01:16 -0400

    Feat messages

--- src/components/PlayGround.tsx
+++ src/components/PlayGround.tsx
@@ -1,9 +1,17 @@
+import { type ResolveMessage } from "../types";
 import Message from "../elements/Message";
 
-const PlayGround = () => {
+interface PlayGroundProps {
+    resolve: ResolveMessage;
+}
+
+const PlayGround = ({ resolve }: PlayGroundProps) => {
+    const { content, role } = resolve;
+    const imageUrl = role === "assistant" ? "/favicon.webp" : "/favicon.webp";
+
     return (
-        <div className="relative overflow-auto">
-            <Message />
+        <div className="relative overflow-auto flex flex-col gap-4 pr-4">
+            <Message content={content} imageUrl={imageUrl} />
         </div>
     );
 };
--- src/elements/Message.tsx
+++ src/elements/Message.tsx
@@ -1,11 +1,13 @@
-export const Message = () => {
+interface MessageProps {
+    content: string;
+    imageUrl: string;
+}
+
+export const Message = ({ content, imageUrl }: MessageProps) => {
     return (
         <div className="flex flex-col gap-4 p-2 bg-neutral-900 rounded-xl backdrop-blur-3xl">
-            <img className="w-10 h-10 object-cover rounded" src="/favicon.webp" alt="" />
-            <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error omnis tempora placeat obcaecati esse beatae repudiandae magni odit, iusto ut quisquam excepturi temporibus sint earum,
-                laborum iure voluptas voluptatibus necessitatibus?
-            </p>
+            <img className="w-10 h-10 object-cover rounded" src={imageUrl} alt={content} />
+            <p>{content}</p>
         </div>
     );
 };
